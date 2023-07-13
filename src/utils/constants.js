18304e4c34cefa50e285333f226d6fe899803fdd
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-07-13 10:22:13 +0300

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,32 @@
+export const navTabs = [
+    {
+        id: 1,
+        title: 'Cпециализация'
+    },
+    {
+        id: 2,
+        title: 'Образование'
+    },
+    {
+        id: 3,
+        title: 'Контакты'
+    },
+]
+
+export const contacts = [
+    {
+        id: 1,
+        title: 'Телефон',
+        content: '+7 903 168 6387'
+    },
+    {
+        id: 2,
+        title: 'E-mail',
+        content: 'senkir@mail.ru'
+    },
+    {
+        id: 3,
+        title: 'Telegram',
+        content: 'link'
+    },
+]
