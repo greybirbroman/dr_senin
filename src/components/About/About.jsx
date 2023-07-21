66ed3999c6bd2486b8ca0c60fc2eb5be93e78e0d
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-07-21 20:02:57 +0300

    refactor About / webp mainImage

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
-'use client'
+'use client';
+import React from 'react';
 import styles from './About.module.css';
 import List from '../List/List';
 import { motion as m } from 'framer-motion';
@@ -12,31 +13,45 @@ const About = () => {
   return (
     <section className={styles.section} id='about'>
       <SectionTitle title='обо мне' />
-      {info.map((section, index) => (
-        <ul key={section.id} className={styles.list} id={section.id === 1 ? 'specialization' : 'education'}>
+      {info.map((section) => (
+       <React.Fragment key={section.id}>
           <m.h3
-          variants={titleVarians}
-          initial='hidden'
-          whileInView='show'
-          className={`${styles.sectionTitle} ${
-            section.id === 1 ? styles.sectionTitleColor : ''
-          }`}>{section.title}</m.h3>
+            variants={titleVarians}
+            initial='hidden'
+            whileInView='show'
+            className={`${styles.sectionTitle} ${
+              section.id === 1 ? styles.sectionTitleColor : ''
+            }`}
+          >
+            {section.title}
+          </m.h3>
+          <ul
+            className={styles.list}
+            id={section.id === 1 ? 'specialization' : 'education'}
+          >
             <li className={section.id === 1 ? styles.item : styles.flex}>
               {section.id === 1 && (
                 <>
-                <Image
-                  src={section.image}
-                  quality={100}
-                  sizes='(max-width: 1600px) 50vw, 33vw'
-                  alt='Изображение, зубы, протезирование'
-                  className={styles.image}
-                />
-                <Link href="https://ru.freepik.com/free-photo/top-view-over-career-guidance-items-for-dentists_27641893.htm#page=6&query=dental%20sergery&position=12&from_view=search&track=ais" className={styles.freepik} target='_blank'>Изображение от Freepik</Link>
+                  <Image
+                    src={section.image}
+                    quality={100}
+                    sizes='(max-width: 1600px) 50vw, 33vw'
+                    alt='Изображение, зубы, протезирование'
+                    className={styles.image}
+                  />
+                  <Link
+                    href='https://ru.freepik.com/free-photo/top-view-over-career-guidance-items-for-dentists_27641893.htm#page=6&query=dental%20sergery&position=12&from_view=search&track=ais'
+                    className={styles.freepik}
+                    target='_blank'
+                  >
+                    Изображение от Freepik
+                  </Link>
                 </>
               )}
               <List section={section} />
             </li>
-        </ul>
+          </ul>
+        </React.Fragment>
       ))}
     </section>
   );
